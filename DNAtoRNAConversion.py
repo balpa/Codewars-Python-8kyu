c670e45c4798a0b1e730e2016b394c2776b4b546
balpa <44147280+balpa@users.noreply.github.com>
2021-06-02 22:36:11 +0300

    Create DNAtoRNAConversion.py

---
+++ DNAtoRNAConversion.py
@@ -0,0 +1,8 @@
+"""Deoxyribonucleic acid, DNA is the primary information storage molecule in biological systems. It is composed of four nucleic acid bases Guanine ('G'), Cytosine ('C'), Adenine ('A'), and Thymine ('T').
+
+Ribonucleic acid, RNA, is the primary messenger molecule in cells. RNA differs slightly from DNA its chemical structure and contains no Thymine. In RNA Thymine is replaced by another nucleic acid Uracil ('U').
+
+Create a function which translates a given DNA string into RNA."""
+
+def dna_to_rna(dna):
+    return dna.replace("T","U") if "T" in dna else dna
