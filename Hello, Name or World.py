71a693e819bf7c562fd0917e589af850c07c11a9
balpa <44147280+balpa@users.noreply.github.com>
2021-06-02 21:37:08 +0300

    Create Hello, Name or World.py

---
+++ Hello, Name or World.py
@@ -0,0 +1,6 @@
+"""Define a method hello that returns "Hello, Name!" to a given name, or says Hello, World! if name is not given (or passed as an empty String).
+
+Assuming that name is a String and it checks for user typos to return a name with a first capital letter (Xxxx)."""
+
+def hello(name=""):
+    return f"Hello, {name.capitalize()}!" if name !="" else "Hello, World!"
