f0102e6d69687cdfaeacc216fad5ac0aa9a7ed73
balpa <44147280+balpa@users.noreply.github.com>
2021-06-02 21:34:14 +0300

    Create Return Negative.py

---
+++ Return Negative.py
@@ -0,0 +1,4 @@
+"""In this simple assignment you are given a number and have to make it negative. But maybe the number is already negative?"""
+
+def make_negative( number ):
+    return -1*number if number > 0 else number
